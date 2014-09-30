0bc2877423b45e9cd0efbef8207cdf5d1563b5af
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2014-09-30 17:09:20 +0300

    Added tests for UsersController

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -1,5 +1,5 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe 'BoardsController', :type => :controller do
+RSpec.describe BoardsController, :type => :controller do
 
 end #OfBoard
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
-RSpec.configure do |config|
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment",__FILE__)
+require 'rspec/rails'
 
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
 end
